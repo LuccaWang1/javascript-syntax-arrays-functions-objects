e0c0907edb5b41269489f15b274852075f381e43
Lucca Wang <luccawang@gmail.com>
2023-12-18 19:16:41 -0700

    fuctions.js complete

--- functions.js
+++ functions.js
@@ -1,13 +1,43 @@
-'use strict';
+"use strict";
 
 // 1. isHometown
 
-// Define your function here
+function isHometown(town) {
+  if (town === "San Francisco") {
+    return console.log(true);
+  } else {
+    return console.log(false);
+  }
+}
+// isHometown("Walnut Creek");
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(first_name, last_name) {
+  const full_name_array = [first_name, last_name];
+
+  console.log(full_name_array.join(" "));
+  // console.log(`${first_name} ${last_name}`); // This is another way that works!!!
+}
+// getFullName("Lucca", "Wang");
 
 // 3. calculateTotal
+function calculateTotal(basePrice, state, tax = 0.05) {
+  const subtotal = basePrice * (1 + tax);
+  let fee = 0;
 
-// Define your function here
+  if (state == "CA") {
+    let fee = 0.03 * subtotal;
+  } else if (state == "PA") {
+    let fee = 2;
+  } else if (state == "MA");
+  {
+    if (basePrice <= 100) {
+      let fee = 1;
+    } else {
+      let fee = 3;
+    }
+  }
+  console.log(Number(subtotal) + Number(fee));
+}
+calculateTotal(5, "CA");
